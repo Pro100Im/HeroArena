c8b4ffad64205293b3d6eb0162ef57a862f6c678
Elite <pro100elite@gmail.com>
2026-02-01 22:02:59 +0100

    Menu update, and some changes

--- src/HeroArena/Assets/Code/Meta/UI/HUD/HomeHUD.cs
+++ src/HeroArena/Assets/Code/Meta/UI/HUD/HomeHUD.cs
@@ -2,7 +2,10 @@
 using Code.Common.Windows;
 using Code.Infrastructure.Loading;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using VContainer;
 
@@ -10,19 +13,25 @@ namespace Code.Meta.UI.HUD
 {
     public class HomeHUD : MonoBehaviour
     {
+        [SerializeField] private float _fadeDuration = 0.2f;
         [SerializeField] private string _gameSceneName = "Game";
         [Space]
         [SerializeField] private Button _startBattleButton;
+        [Space]
+        [SerializeField] private CanvasGroup _introGroup;
+        [SerializeField] private CanvasGroup _menuGroup;
+        [Space]
+        [SerializeField] private InputActionMap _pressAnyBtn;
 
         private ISceneLoader _sceneLoader;
-        private IWindowService _windowService;
         private TransitionService _transitionService;
 
+        public IObserver<InputControl> OnAnyButton { get; private set; }
+
         [Inject]
-        private void Construct(ISceneLoader sceneLoader, IWindowService windowService, TransitionService transitionService)
+        private void Construct(ISceneLoader sceneLoader, TransitionService transitionService)
         {
             _sceneLoader = sceneLoader;
-            _windowService = windowService;
             _transitionService = transitionService;
         }
 
@@ -30,6 +39,23 @@ namespace Code.Meta.UI.HUD
         {
             _startBattleButton.onClick.AddListener(EnterBattleLoadingState);
             _transitionService.Execute(0).AsTask();
+            _pressAnyBtn.actionTriggered += OnAnyButtonPress;
+            _pressAnyBtn.Enable();
+        }
+
+        private void OnAnyButtonPress(InputAction.CallbackContext context)
+        {
+            if (context.canceled)
+            {
+                _pressAnyBtn.Disable();
+                _pressAnyBtn.actionTriggered -= OnAnyButtonPress;
+
+                _introGroup.DOFade(0, _fadeDuration);
+                _introGroup.blocksRaycasts = false;
+
+                _menuGroup.DOFade(1, _fadeDuration);
+                _menuGroup.blocksRaycasts = true;
+            }
         }
 
         private async void EnterBattleLoadingState()
