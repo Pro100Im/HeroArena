d70efe7e140bd618dbb58b1c6d5038e72bef6cd9
Elite <pro100elite@gmail.com>
2026-02-01 10:11:50 +0100

    Camera

--- src/HeroArena/Assets/Settings/Input/NewInputSystemApi.cs
+++ src/HeroArena/Assets/Settings/Input/NewInputSystemApi.cs
@@ -99,7 +99,7 @@ namespace Settings.Input
                     ""type"": ""Value"",
                     ""id"": ""351f2ccd-1f9f-44bf-9bec-d62ac5c5f408"",
                     ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
+                    ""processors"": ""NormalizeVector2"",
                     ""interactions"": """",
                     ""initialStateCheck"": true
                 },
@@ -108,7 +108,7 @@ namespace Settings.Input
                     ""type"": ""Value"",
                     ""id"": ""6b444451-8a00-4d00-a97e-f47457f736a8"",
                     ""expectedControlType"": ""Vector2"",
-                    ""processors"": """",
+                    ""processors"": ""NormalizeVector2"",
                     ""interactions"": """",
                     ""initialStateCheck"": true
                 },
